0129a67ed1d6b8f6dbd1e29c929de2491e13974f
Sean Neden <sean.neden+github@gmail.com>
2014-10-10 00:14:32 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Issanfranciscofoggy::Application.config.secret_token = '162888d3dbc286243bffb7ba948df4bf6a72eb6667477ad92cec96b111711f9d4b95017eaeed781d8f2426469571c9d06107771051feb96c81bfd3ec07d98dc9'
