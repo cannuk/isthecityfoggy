b0aa005474903ed338edfdcfe246da938ccc8b9f
Sean Neden <sean.neden+github@gmail.com>
2014-10-13 17:45:58 -0700

    exclude id and created_at from json output

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -1,3 +1,7 @@
 class Hour < ActiveRecord::Base
    attr_accessible :foggy, :hour, :created_at, :updated_at
+
+   def as_json(options={})
+     super({ except: [:id, :created_at] }.merge(options || {}))
+   end
 end
