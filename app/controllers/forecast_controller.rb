358365044d58eb206f58f68e217205b33d7b908e
Sean Neden <sean.neden+github@gmail.com>
2014-10-16 14:43:26 -0700

    remove array from current

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -6,7 +6,7 @@ class ForecastController < ApplicationController
 
   def current
     hour = Time.new().hour
-    render :json => Hour.where(:hour => hour).order(:hour).to_json, :callback => params['callback']
+    render :json => Hour.where(:hour => hour).order(:hour).first.to_json(), :callback => params['callback']
   end
 
   def time
